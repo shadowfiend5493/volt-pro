e902423c033b6067eb04d8ab1d10360dcad7ca34
cadbury5493 <sarthaks5493@gmail.com>
2026-07-27 22:05:13 +0100

    Refactor VoltstoreApplication to include logging and remove unused YAML and Java files. Added logging for application startup and removed deprecated configuration files and controller/service classes.

--- voltstore/src/main/java/com/voltpro/voltstore/VoltstoreApplication.java
+++ voltstore/src/main/java/com/voltpro/voltstore/VoltstoreApplication.java
@@ -1,5 +1,7 @@
 package com.voltpro.voltstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,8 +10,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class VoltstoreApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(VoltstoreApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("Starting VoltPro Store application");
 		SpringApplication.run(VoltstoreApplication.class, args);
+		LOGGER.info("VoltPro Store application started successfully");
 	}
 
 }
